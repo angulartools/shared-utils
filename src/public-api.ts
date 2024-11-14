668d5ffd0be58f6dc0c439f72875c09f8a61cec1
danieleespindola-brenntag <daniele.espindola@brenntag-external.com>
2024-11-14 14:13:01 -0300

    ajuste no package.json

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,5 +2,10 @@
  * Public API Surface of shared-utils
  */
 
-export * from './lib/shared-utils.service';
-export * from './lib/shared-utils.component';
+export * from './lib/pipes/cpf-cnpj-format.pipe';
+export * from './lib/pipes/date-format.pipe';
+export * from './lib/pipes/replace-null-with-text.pipe';
+export * from './lib/pipes/safe.pipe';
+export * from './lib/pipes/utcDate.pipe';
+export * from './lib/mask/mask';
+export * from './lib/services/alert.service';
